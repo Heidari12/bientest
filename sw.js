1bd9b64ca832882a892de39c5e9a44518f419b4f
Heidari12 <h.h.moghaddam.a@gmail.com>
2026-09-19 20:26:25 +0330

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='bien-board-v5';
+const CACHE='bien-board-v6';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./logo.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim().then(()=>caches.keys()).then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
